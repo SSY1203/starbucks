617864af9920acf7773f762822cdadb3f4a02c0c
SSY1203 <tlstjsdud566@naver.com>
2023-12-10 19:19:46 +0900

    [feat] lodash 및 gsap을 이용한 배지 UI 및 애니메이션 구현

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ searchInputElement.addEventListener('blur', () => {
   searchElement.classList.remove('focused');
   searchInputElement.setAttribute('placeholder', '');
 });
+
+const badgeElement = document.querySelector('header .badges');
+
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      // TODO 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeElement, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      // TODO 배지 보이기
+      gsap.to(badgeElement, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300),
+);
+// _.throttle(함수, 시간)
