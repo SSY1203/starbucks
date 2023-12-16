87154ae1e710510e4e1bb6c4711fdd8e1587390a
SSY1203 <tlstjsdud566@naver.com>
2023-12-16 16:33:09 +0900

    [feat] gsap을 이용한 페이지 상단으로 이동 구현

--- js/main.js
+++ js/main.js
@@ -16,27 +16,44 @@ searchInputElement.addEventListener('blur', () => {
 });
 
 const badgeElement = document.querySelector('header .badges');
+const toTopElement = document.querySelector('#to-top');
 
 window.addEventListener(
   'scroll',
   _.throttle(() => {
     if (window.scrollY > 500) {
-      // TODO 배지 숨기기
+      // 배지 숨기기
       // gsap.to(요소, 지속시간, 옵션);
       gsap.to(badgeElement, 0.6, {
         opacity: 0,
         display: 'none',
       });
+
+      // 버튼 보이기
+      gsap.to(toTopElement, 0.2, {
+        x: 0,
+      });
     } else {
-      // TODO 배지 보이기
+      // 배지 보이기
       gsap.to(badgeElement, 0.6, {
         opacity: 1,
         display: 'block',
+      });
+
+      // 버튼 숨기기
+      gsap.to(toTopElement, 0.2, {
+        x: 100,
       });
     }
   }, 300),
 );
 // _.throttle(함수, 시간)
+
+toTopElement.addEventListener('click', () => {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeElements = document.querySelectorAll('.visual .fade-in');
 
